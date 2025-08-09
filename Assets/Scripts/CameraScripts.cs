d0f2a4fe95f8f518a959ef9a326b20406e77d120
Tyler4S <tyler.snow455@gmail.com>
2025-08-09 10:43:41 +0200

    Added camera movement

--- Assets/Scripts/CameraScripts.cs
+++ Assets/Scripts/CameraScripts.cs
@@ -2,18 +2,45 @@ using UnityEngine;
 
 public class CameraScripts : MonoBehaviour
 {
-
-    public Camera mainCamera;
     public GameObject player1;
     public GameObject player2;
+
+    public float deadzonesX;
+    public float deadzonesY;
+
     void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
+        float x1 = player1.transform.position.x;
+        float x2 = player2.transform.position.x;
+        float maxX = Mathf.Max(x1, x2);
+        float minX = Mathf.Min(x1, x2);
+        float y1 = player1.transform.position.y;
+        float y2 = player2.transform.position.y;
+        float maxY = Mathf.Max(y1, y2);
+        float minY = Mathf.Min(y1, y2);
         
+        if (maxX >= transform.position.x + deadzonesX)
+        {
+            transform.position = new Vector3(maxX - deadzonesX, transform.position.y, transform.position.z);
+        }
+        else if (minX < transform.position.x - deadzonesX && maxX < transform.position.x + deadzonesX)
+        {
+            transform.position = new Vector3(maxX > transform.position.x+ deadzonesX-0.2f ? maxX - deadzonesX: minX+deadzonesX, transform.position.y, transform.position.z);
+        } 
+        
+        if (maxY >= transform.position.y + deadzonesY)
+        {
+            transform.position = new Vector3(transform.position.x, maxY - deadzonesY, transform.position.z);
+        }
+        else if (minY < transform.position.y - deadzonesY && maxY < transform.position.y + deadzonesY)
+        {
+            transform.position = new Vector3(transform.position.x, maxY > transform.position.y + deadzonesY -0.2f ? maxY - deadzonesY : minY+deadzonesY, transform.position.z);
+        }
     }
 }
